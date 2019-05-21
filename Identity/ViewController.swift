ece4fa0de975b9d4f12a080c85bc5946e44447d1
Or Israeli <isra.or@gmail.com>
2019-05-21 19:56:22 +0300

    v2 build 2

--- Identity/ViewController.swift
+++ Identity/ViewController.swift
@@ -10,19 +10,16 @@ import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
     
-    
     @IBOutlet weak var infoText: UITextView!
     @IBOutlet weak var idInput: UITextField!
     @IBOutlet weak var resultView: UIView!
-        @IBOutlet weak var resultLabel: UILabel!
-        @IBOutlet var signImageView: UIImageView!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet var signImageView: UIImageView!
     @IBOutlet weak var creditsText: UITextView!
     
     let checkmarkSign = UIImage(named:"checkmark")
     let xSign = UIImage(named:"x")
-    
-    //int array to hold id number
-    var idArray = [Int]()
+    var idString = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,72 +28,49 @@ class ViewController: UIViewController, UITextFieldDelegate {
         resultLabel.textColor = UIColor.white
         infoText.linkTextAttributes = [.foregroundColor: UIColor.lightGray]
         creditsText.linkTextAttributes = [.foregroundColor: UIColor.lightGray]
-        initParameters()
     }
     
-    //clears data
-    func initParameters() {
-        resultLabel.text = nil
-        idArray = []
-    }
     
-    //check button event
+    //button event
     @IBAction func didPressCheck(_ sender: Any) {
-        initParameters()
+        
         //read text input
-        var idString = idInput.text!
-        let idNumber = Int(idString)
-        var fillerCount = 0
+        idString = idInput.text!
         
-        func addToArray(){
-            //if string conatains something other than numbers
-            if idNumber == nil {
-                resultLabel.text = "Please enter only numeral characters."
-                print("error: string is nil")
-                resultView.backgroundColor = UIColor(red: 259/255.0, green: 21/255.0, blue: 29/255.0, alpha: 1.0)
-                signImageView.image = xSign
-            }
-            else {
-                print("string is ok")
-                //adds to array
-                for char in idString {
-                    idArray.append(Int(String(char))!)
-                }
-                print("    id: \(idArray)")
-                //validate
-                validateID()
-                //dismiss keyboard
-                idInput.resignFirstResponder()
-            }
-        }
-        
-        if idString.isEmpty {
+        //check if string is ok
+        if Int(idString) == nil || idString.isEmpty || idString.count < 5 || idString.count > 9 {
+            resultLabel.text = "ERROR"
+            print("error: string is bad")
             resultView.backgroundColor = UIColor(red: 259/255.0, green: 21/255.0, blue: 29/255.0, alpha: 1.0)
             signImageView.image = xSign
-            resultLabel.text = "Please enter an ID number."
-            print("error: no value")
         }
-            //check strings length
-        else if idString.count < 5 || idString.count > 9 {
-            resultView.backgroundColor = UIColor(red: 259/255.0, green: 21/255.0, blue: 29/255.0, alpha: 1.0)
-            signImageView.image = xSign
-            resultLabel.text = "Required length 5-9 characters."
-            print("error: id is not in range")
-        }
-            //if string is shorter than 9, fill it with zero's to length
+            
         else {
-            fillerCount = 9-idString.count
-            while fillerCount > 0 {
-                fillerCount -= 1
-                idString = "0" + idString
-            }
-            addToArray()
+            printResult()
+            //dismiss keyboard
+            idInput.resignFirstResponder()
+        }
+    
+    }
+    
+    func validateID() -> Bool {
+        
+         var idArray = [Int]()
+    
+        //if string is shorter than 9, fill it with zero's to length
+        var fillerCount = 9-idString.count
+        while fillerCount > 0 {
+            fillerCount -= 1
+            idString = "0" + idString
             idInput.text = idString
         }
-    }
-    
-    //validation algorithm
-    func validateID() {
+        
+        //adds to array
+        for char in idString {
+            idArray.append(Int(String(char))!)
+        }
+        
+        //validation algorithm
         var i = 0
         var total = 0
         for element in idArray {
@@ -114,21 +88,32 @@ class ViewController: UIViewController, UITextFieldDelegate {
         }
         print("result: \(idArray)")
         print("total: \(total)")
-        
-        //print result
-        if total % 10 == 0 {
-            resultView.backgroundColor = UIColor(red: 39/255.0, green: 200/255.0, blue: 45/255.0, alpha: 1.0)
-            signImageView.image = checkmarkSign
-            resultLabel.text = "The ID number is valid."
-            print("id valid")
+        idArray = []
+    
+        if total > 0 && total % 10 == 0 {
+            return true
         }
+            
         else {
-            resultView.backgroundColor = UIColor(red: 259/255.0, green: 21/255.0, blue: 29/255.0, alpha: 1.0)
-            signImageView.image = xSign
-            resultLabel.text = "The ID number is invalid."
-            print("id invalid")
+            return false
         }
     }
+    
+        func printResult(){
+            if validateID() {
+                resultView.backgroundColor = UIColor(red: 39/255.0, green: 200/255.0, blue: 45/255.0, alpha: 1.0)
+                signImageView.image = checkmarkSign
+                resultLabel.text = "The ID number is valid."
+                print("id valid")
+            }
+            else {
+                resultView.backgroundColor = UIColor(red: 259/255.0, green: 21/255.0, blue: 29/255.0, alpha: 1.0)
+                signImageView.image = xSign
+                resultLabel.text = "The ID number is invalid."
+                print("id invalid")
+            }
+        }
+   
     
     //dismiss keyboard when user touch blank spots
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -142,7 +127,5 @@ class ViewController: UIViewController, UITextFieldDelegate {
         return true
     }
     
-
-
 }
 
